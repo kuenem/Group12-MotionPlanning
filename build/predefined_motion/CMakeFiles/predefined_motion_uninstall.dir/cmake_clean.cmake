4247d6ee7301184c4440ff62078942a55985d150
kuenem <e.kunstner@student.maastrichtuniversity.nl>
2025-05-28 13:53:06 +0200

    Building Version

---
+++ build/predefined_motion/CMakeFiles/predefined_motion_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/predefined_motion_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/predefined_motion_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
