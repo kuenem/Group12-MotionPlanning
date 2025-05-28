363231187e55722783b45a86d81f2ed58d711a4a
kuenem <e.kunstner@student.maastrichtuniversity.nl>
2025-05-28 16:23:44 +0200

    Last not working Version

--- install/group_12_motion_planning/share/group_12_motion_planning/package.sh
+++ install/group_12_motion_planning/share/group_12_motion_planning/package.sh
@@ -78,7 +78,6 @@ _colcon_package_sh_source_script() {
 }
 
 # source sh hooks
-_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/group_12_motion_planning/hook/cmake_prefix_path.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/group_12_motion_planning/local_setup.sh"
 
 unset _colcon_package_sh_source_script
--- src/group_12_motion_planning/src/ur5e_moveit_controller.cpp
+++ src/group_12_motion_planning/src/ur5e_moveit_controller.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
-#include <moveit/move_group_interface/move_group_interface.h>
+// #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit_ros_planning_interface/moveit/move_group_interface/move_group_interface.h>
 #include <geometry_msgs/msg/pose.hpp>
 
 int main(int argc, char *argv[])
